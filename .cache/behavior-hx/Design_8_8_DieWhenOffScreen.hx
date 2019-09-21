4fa21f0c1188068c15add2ff77cee8795cc1cddf
MGVoltage <MGVoltage@users.noreply.github.com>
2019-09-21 17:07:46 -0400

    save point 9
    Bullet types have their uniqueness, boss collision works, and each bullet does different damage amounts. Hitboxes have also been adjusted.

--- .cache/behavior-hx/Design_8_8_DieWhenOffScreen.hx
+++ .cache/behavior-hx/Design_8_8_DieWhenOffScreen.hx
@@ -61,79 +61,31 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class Design_0_0_AlienMovement extends ActorScript
+class Design_8_8_DieWhenOffScreen extends ActorScript
 {
-	public var _MovementSpeed:Float;
-	public var _SlidandIncreasedforSelf:Bool;
 	
 	
 	public function new(dummy:Int, actor:Actor, dummy2:Engine)
 	{
 		super(actor);
 		nameMap.set("Actor", "actor");
-		nameMap.set("Movement Speed", "_MovementSpeed");
-		_MovementSpeed = 2.0;
-		nameMap.set("Slid and Increased for Self", "_SlidandIncreasedforSelf");
-		_SlidandIncreasedforSelf = false;
 		
 	}
 	
 	override public function init()
 	{
 		
+		/* ======================== When Creating ========================= */
+		actor.makeAlwaysSimulate();
+		
 		/* ======================== When Updating ========================= */
 		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
 		{
 			if(wrapper.enabled)
 			{
-				/* Reached an edge. Slide down towards the
-player and increase the movement speed. */
-				if((Engine.engine.getGameAttribute("Slide and Increase Speed") : Bool))
+				if(!(actor.isOnScreen()))
 				{
-					if(!(_SlidandIncreasedforSelf))
-					{
-						_SlidandIncreasedforSelf = true;
-						actor.moveBy(0, 16, 0.2, Easing.linear);
-						_MovementSpeed = (_MovementSpeed + 1);
-						runLater(1000 * 0.02, function(timeTask:TimedTask):Void
-						{
-							Engine.engine.setGameAttribute("Slide and Increase Speed", false);
-							_SlidandIncreasedforSelf = false;
-						}, actor);
-					}
-				}
-				/* Reached the left side. Switch direction. */
-				if((actor.getX() <= 2))
-				{
-					if(!((Engine.engine.getGameAttribute("Move Right") : Bool)))
-					{
-						Engine.engine.setGameAttribute("Slide and Increase Speed", true);
-						Engine.engine.setGameAttribute("Move Right", true);
-					}
-				}
-				/* Reached the right side. Switch direction. */
-				if(((actor.getX() + (actor.getWidth())) >= (getSceneWidth() - 2)))
-				{
-					if((Engine.engine.getGameAttribute("Move Right") : Bool))
-					{
-						Engine.engine.setGameAttribute("Slide and Increase Speed", true);
-						Engine.engine.setGameAttribute("Move Right", false);
-					}
-				}
-				/* Make the aliens move left or right
-at a constant rate. */
-				if((Engine.engine.getGameAttribute("Move Right") : Bool))
-				{
-					actor.setXVelocity(_MovementSpeed);
-				}
-				else
-				{
-					actor.setXVelocity(-(_MovementSpeed));
-				}
-				/* They reached the end. You die. */
-				if((actor.getY() >= (getSceneHeight() - 159)))
-				{
-					reloadCurrentScene(createFadeOut(0.3), createFadeIn(0.3));
+					recycleActor(actor);
 				}
 			}
 		});
